cd8be6e8168d6b957ae56cfcb574bd9e70b27b2e
Ethan Williams <ethan555williams@gmail.com>
2020-10-01 00:25:13 -0500

    First Commit, camera controls and basic terrain generation

---
+++ Assets/CameraController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class CameraController : MonoBehaviour
+{
+    #region Camera
+        public static CameraController instance;
+        public Transform cameraTransform;
+        public Vector3 zoomAmount;
+        private Vector3 zoom;
+        private Vector3 zoomVelocity;
+        public float minZoom;
+        public float maxZoom;
+        public float zoomRate;
+    #endregion
+    #region Panning variables
+        public float panSpeed;
+        public float panBorderThickness;
+        public float panRate;
+        public Vector2 panLimit;
+    #endregion
+    #region Position/Rotation/Movement
+        private Vector3 position;
+        private Vector3 velocity;
+        private Quaternion rotation;
+        private Quaternion deriv;
+        public bool rotateSmooth = false;
+        public float rotationSetAmount;
+        public float rotationSmoothAmount;
+        public float rotateRate;
+    #endregion
+
+    private void Start() {
+        instance = this;
+        position = transform.position;
+        rotation = transform.rotation;
+        zoom = cameraTransform.localPosition;
+    }
+
+    private void Update() {
+        HandleInput();
+
+        position.x = Mathf.Clamp(position.x, -panLimit.x, panLimit.x);
+        position.z = Mathf.Clamp(position.z, -panLimit.y, panLimit.y);
+        zoom.y = Mathf.Clamp(zoom.y, minZoom, maxZoom);
+        zoom.z = Mathf.Clamp(zoom.z, -maxZoom, -minZoom);
+
+        transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, panRate);
+        transform.rotation = QuaternionUtil.SmoothDamp(transform.rotation, rotation, ref deriv, rotateRate);
+
+        cameraTransform.localPosition = Vector3.SmoothDamp(cameraTransform.localPosition, zoom, ref zoomVelocity, zoomRate);
+    }
+
+    private void HandleInput() {
+        Vector3 diff;
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - panBorderThickness) {
+            diff = transform.forward * panSpeed * Time.deltaTime;
+            position += diff;
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= panBorderThickness) {
+            diff = transform.forward * panSpeed * Time.deltaTime;
+            position -= diff;
+        }
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - panBorderThickness) {
+            diff = transform.right * panSpeed * Time.deltaTime;
+            position += diff;
+        }
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= panBorderThickness) {
+            diff = transform.right * panSpeed * Time.deltaTime;
+            position -= diff;
+        }
+        if (Input.GetKeyDown("p")) {
+            rotateSmooth = !rotateSmooth;
+        }
+        if (!rotateSmooth) {
+            if (Input.GetKeyDown("q")) {
+                rotation *= Quaternion.Euler(Vector3.up * rotationSetAmount);
+            }
+            if (Input.GetKeyDown("e")) {
+                rotation *= Quaternion.Euler(Vector3.up * -rotationSetAmount);
+            }
+        } else {
+            if (Input.GetKey("q")) {
+                rotation *= Quaternion.Euler(Vector3.up * rotationSmoothAmount);
+            }
+            if (Input.GetKey("e")) {
+                rotation *= Quaternion.Euler(Vector3.up * -rotationSmoothAmount);
+            }
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoom += scroll * zoomAmount;
+    }
+}
---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    Color[] colors;
+
+    public int xSize;
+    public int zSize;
+
+    public float xOffset;
+    public float zOffset;
+
+    public float width;
+    public float length;
+
+    public float height;
+    public float terraceHeight;
+    public float scale1;
+    public float scale2;
+    public float scale3;
+    public float octave1;
+    public float octave2;
+    public float octave3;
+    public float waterHeightRatio;
+
+    public MeshFilter meshFilter;
+    public Material material;
+    public Gradient gradient;
+
+    // Start is called before the first frame update
+    void Start() {
+        mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        meshFilter.mesh = mesh;
+        Vector3 position = new Vector3(-xSize * width / 2f, 0f, -zSize * length / 2f);
+        meshFilter.transform.Translate(position, Space.World);
+
+        // material.SetFloat("TerraceHeight", terraceHeight);
+        material.SetFloat("Height", height);
+
+        // StartCoroutine(IEnumerable CreateShapeCoroutine());
+        GenerateLand();
+        UpdateMesh();
+    }
+    
+    // private void Update() {
+    //     UpdateMesh();
+    // }
+
+    void GenerateLand() {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int z = 0, i = 0; z < zSize + 1; z++) {
+            for (int x = 0; x < xSize + 1; x++, i++) {
+                float y = 1f;
+                if (terraceHeight > 0) {
+                    y = getHeightTerraced(x, z);
+                } else {
+                    y = getHeightOctaves(x, z);
+                }
+                vertices[i] = new Vector3(x * width, y, z * length);
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+        for (int z = 0; z < zSize; z++, vert++) {
+            for (int x = 0; x < xSize; x++, vert++, tris += 6) {
+                triangles[tris] = vert;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+            }
+            // UpdateMesh();
+            // yield return 0;
+        }
+
+        colors = new Color[vertices.Length];
+
+        for (int z = 0, i = 0; z < zSize + 1; z++) {
+            for (int x = 0; x < xSize + 1; x++, i++) {
+                // uvs[i] = new Vector2((float) x / xSize, (float) z / zSize);
+                float colorHeight = vertices[i].y / height;
+                colors[i] = gradient.Evaluate(colorHeight);
+                // Debug.Log("GRADIENT: " + colorHeight + " VERTEX: " + i + " COLOR: " + colors[i].ToString());
+            }
+        }
+    }
+
+    float getHeightOctaves(float x, float z) {
+        float y = Mathf.Max(height * (
+            octave1 * Mathf.PerlinNoise((x + xOffset) * scale1, (z + zOffset) * scale1) +
+            octave2 * Mathf.PerlinNoise((x + xOffset) * scale2, (z + zOffset) * scale2) +
+            octave3 * Mathf.PerlinNoise((x + xOffset) * scale3, (z + zOffset) * scale3)
+            ), waterHeightRatio * height);
+        return y;
+    }
+
+    float getHeightTerraced(float x, float z) {
+        float y = Mathf.Floor(Mathf.Max(height * (
+            octave1 * Mathf.PerlinNoise((x + xOffset) * scale1, (z + zOffset) * scale1) +
+            octave2 * Mathf.PerlinNoise((x + xOffset) * scale2, (z + zOffset) * scale2) +
+            octave3 * Mathf.PerlinNoise((x + xOffset) * scale3, (z + zOffset) * scale3)
+            ), waterHeightRatio * height) / terraceHeight) * terraceHeight;
+        return y;
+    }
+
+    void UpdateMesh() {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+
+        mesh.RecalculateNormals();
+    }
+
+    // private void OnDrawGizmos() {
+    //     if (vertices == null) {
+    //         return;
+    //     }
+    //     Vector3 drawPosition = meshFilter.transform.position;
+    //     for (int i = 0; i < vertices.Length; i++) {
+    //         Gizmos.color = mesh.colors[i];
+    //         Gizmos.DrawSphere(drawPosition + vertices[i], .1f);
+    //     }
+    // }
+}
